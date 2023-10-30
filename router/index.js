341b10f89102adb0e8a570c4333b6071c2ea0ddf
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-10-30 21:02:05 +0700

    make dosen, matkul, dosen matkul route api

--- router/index.js
+++ router/index.js
@@ -1,7 +1,15 @@
-const express = require("express");
-const exampleController = require("../controller/exampleController");
-const route = express.Router()
+const express = require('express');
+const exampleController = require('../controller/exampleController');
+const routeMahasiswa = require('./mahasiswa');
+const routeDosen = require('./dosen');
+const routeMatkul = require('./matkul');
+const routeDosenMatkul = require('./dosenMatkul');
+const route = express.Router();
 
-route.get('/',exampleController.index)
+route.get('/', exampleController.index);
+route.use('/mahasiswa', routeMahasiswa);
+route.use('/dosen', routeDosen);
+route.use('/matkul', routeMatkul);
+route.use('/dosen-matkul', routeDosenMatkul);
 
-module.exports = route
+module.exports = route;
